f4653847fc802aa025230f79166d04a1c56608bf
Mikhail Golubyatnikov <magolubyatnikov@gmail.com>
2019-07-13 02:00:34 +0300

    add implicit wait

--- pages/base_page.py
+++ pages/base_page.py
@@ -8,10 +8,10 @@ from pages.locators import BasePageLocators
 
 
 class BasePage(object):
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url, timeout=5):
         self.browser = browser
         self.url = url
-        #self.browser.implicitly_wait(timeout)
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
